d9fececfd155ed19b98208bdc9043664b1b0adff
Alejandro <alj70111@gmail.com>
2023-09-28 17:24:31 -0300

    initial commit

---
+++ src/main/java/org/example/Home/LoginPage.java
@@ -0,0 +1,29 @@
+package org.example.Home;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    private WebDriver driver;
+    private By userNameField = By.id("username");
+    private By userPasswordField = By.id("password");
+    private By loginButton  = By.cssSelector("#login button");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void userName(String username){
+        driver.findElement(userNameField).sendKeys(username);
+    }
+    public void userPassword(String password){
+        driver.findElement(userPasswordField).sendKeys(password);
+    }
+
+    public SecureAreaPage clickLoginButoon(){
+        driver.findElement(loginButton).click();
+        return new SecureAreaPage(driver);
+    }
+
+}
---
+++ src/test/java/login/LoginTests.java
@@ -0,0 +1,31 @@
+package login;
+
+import base.BaseTests;
+import org.example.Home.LoginPage;
+import org.example.Home.SecureAreaPage;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class LoginTests extends BaseTests {
+
+    @Test
+    public void testSuccesfulLogin(){
+        LoginPage loginPage = homePage.clickFormAuthentication();
+        loginPage.userName("tomsmith");
+        loginPage.userPassword("SuperSecretPassword!");
+        SecureAreaPage secureAreaPage = loginPage.clickLoginButoon();
+        //AssertEquals:
+        assertEquals(secureAreaPage.getAlertText(),"You logged into a secure area!\n" +
+                "×","Error enb el mensaje");
+        System.out.println(secureAreaPage.getAlertText());
+        //AssertTrue:
+        assertTrue(secureAreaPage.getAlertText().contains("You logged into a secure area!\n" +
+                "×"),"Error enb el mensaje");
+        System.out.println(secureAreaPage.getAlertText());
+
+    }
+
+
+}
